664bedeedca5c8b4ef9d17c5f38fddca94985fb2
jegconnect <jathusanan@gconnect.com.au>
2024-10-28 11:32:49 +0530

    port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000; // Use Heroku's PORT or default to 3000 locally
+  await app.listen(port);
 }
 bootstrap();
